3cce1553e61693298c9855d7aff1ee8587f30cc2
lee-youn <5816654@naver.com>
2024-08-14 23:59:44 +0900

    ⬆️ Chore : 구글 로그인 추가

--- src/pages/Login/components/Login.tsx
+++ src/pages/Login/components/Login.tsx
@@ -1,17 +1,21 @@
 import KaggomLogo from "../../../assets/KaggomLogo.svg"
 import KakaoLoginImage from "../../../assets/Login&Onboard/kakao_login.png"
+import GoogleLoginImage from "../../../assets/Login&Onboard/google_login.png"
 
-function KakaoLogin() {
+function KakaoGoogleLogin() {
     return (
         <>
         <div className="relative w-full h-screen bg-[#5F88FE]">
             <img src={KaggomLogo} alt="첫페이지" className="absolute top-[244px] left-1/2 transform -translate-x-1/2 flex items-center" />
-            <button className="absolute top-[464px] left-1/2 transform -translate-x-1/2 flex items-center">
+            <button className="absolute top-[430px] left-1/2 transform -translate-x-1/2 flex items-center">
                 <img src={KakaoLoginImage} alt="카카오로그인" /> 
+            </button>
+            <button className="absolute top-[500px] left-1/2 transform -translate-x-1/2 flex items-center">
+                <img src={GoogleLoginImage} alt="구글로그인" /> 
             </button>
         </div>
         </>
     );
 }
 
-export default KakaoLogin;
+export default KakaoGoogleLogin;
