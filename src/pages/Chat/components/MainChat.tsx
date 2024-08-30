9e807b0b4aa303e4814745599229b639ec73767e
lee-youn <5816654@naver.com>
2024-08-30 22:07:02 +0900

    👽️ Api : post 주소 테스트

--- src/pages/Chat/components/MainChat.tsx
+++ src/pages/Chat/components/MainChat.tsx
@@ -35,7 +35,7 @@ function MainChat() {
         
 
         try {
-            const response = await axios.post('/api/search',requestData, {
+            const response = await axios.post('http://20.41.121.150:8000/search',requestData, {
             headers: {
                 'Content-Type': 'application/json', // JSON 형식으로 보내는 것을 명시
                 // 필요한 경우 추가적인 헤더 설정
