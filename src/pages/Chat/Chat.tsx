d6514adf81df3c83d94e449e2380d6784e7b55eb
lee-youn <5816654@naver.com>
2024-08-31 02:23:51 +0900

    👽️ Api : chat 시작 url 재설정

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import MainChat from "./components/MainChat";
 import MainHeader from "./components/MainHeader";
 import axios from "axios";
+import https from 'https';
 
 function Chat() {
 
@@ -23,8 +24,31 @@ function Chat() {
         }
     };
 
+    const newChatApi = async () => {
+        try {
+            const response = await axios.post('https://20.41.121.150/new_session',
+                {
+                    snsid: window.SNSID
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    httpsAgent: new https.Agent({
+                        rejectUnauthorized: false, // SSL 인증서 검증을 무시하도록 설정
+                    }),
+                }
+                );
+                console.log('newChatAPI Response:', response.data);
+        } catch (error) {
+            console.error('API 요청 중 오류 발생:', error instanceof Error ? error.message : error);
+            return null;
+        }
+    }
+
     useEffect(() => {
         authApi(); // useEffect 안에서 authApi 호출
+        newChatApi();
     }, []);
 
     return (
