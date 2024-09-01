27ffebfee837e6c583254fa9f1f2c425fb266e7c
lee-youn <5816654@naver.com>
2024-09-01 21:21:32 +0900

    👽️ Api : record 변경

--- src/pages/Record/Record.tsx
+++ src/pages/Record/Record.tsx
@@ -1,6 +1,43 @@
+import { useEffect } from "react";
 import RecordDetail from "./components/RecordDetail";
+import axios from "axios";
 
 function Record() {
+    const authApi = async () => {
+        try {
+            const response = await axios.post('https://kaggom.store/login',
+                {},
+                {
+                    withCredentials: true
+                }
+            );
+            console.log("authApi response: ", response.data);
+
+            window.SNSID = response.data.user.snsid;
+            window.NAME = response.data.user.nick;
+            window.EMAIL = response.data.user.email;
+
+            console.log(window.SNSID);
+            console.log(window.NAME);
+            console.log(window.EMAIL);
+        } catch (error) {
+            console.error('API 요청 중 오류 발생:', error instanceof Error ? error.message : error);
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        const initializeChat = async () => {
+            try {
+                await authApi(); // authApi가 완료될 때까지 대기
+            } catch (error) {
+                console.error('초기화 과정에서 오류 발생:', error);
+            }
+        };
+    
+        initializeChat();
+    }, []);
+    
     return (
         <div 
         className="flex flex-row" // 전체 높이와 너비를 설정
